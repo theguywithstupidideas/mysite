4e19e3e3199bbbea4aabd90bc007fe4c29263d9d
Davide Prestileo <davidepresti.leo252@gmail.com>
2025-03-09 14:14:15 +0100

    content added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from './pages/main/main.component';
-import {CorpoUmanoComponent} from './pages/biologia/corpo-umano/corpo-umano.component';
 import {CelluleStaminaliComponent} from './pages/biologia/corpo-umano/cellule-staminali/cellule-staminali.component';
+import {StuartAgeComponent} from './pages/inglese/stuart-age/stuart-age.component';
 
 export const routes: Routes = [
   { path: 'main', component: MainComponent },
-  { path: 'corpo-umano', component: CorpoUmanoComponent },
   { path: 'cellule-staminali', component: CelluleStaminaliComponent },
+  { path: 'stuart-age', component: StuartAgeComponent },
   { path: '', redirectTo: 'main', pathMatch: 'full' },
 ];
--- src/app/pages/inglese/stuart-age/stuart-age.component.ts
+++ src/app/pages/inglese/stuart-age/stuart-age.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {InfoPageGenericComponent} from '../../../UI/info-page-generic/info-page-generic.component';
+import {TimelineSegmentComponent} from '../../../UI/timeline-segment/timeline-segment.component';
 
 @Component({
   selector: 'app-stuart-age',
-  imports: [],
+  imports: [
+    InfoPageGenericComponent,
+    TimelineSegmentComponent
+  ],
   templateUrl: './stuart-age.component.html',
   styleUrl: './stuart-age.component.css'
 })
